7162bd5e8a69a7e4a71747e96eb2e831d9fc9cfb
Justin Case <jcase@cunninglogic.com>
2012-03-21 23:47:28 -0700

    Untested first commit

---
+++ Gordita.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "GorditaRoot 1.0 by jcase / TeamAndIRC"
+echo "for the VM670 Optimus V"
+echo "March 21st 2012"
+echo "Follow me! http://www.twitter.com/TeamAndIRC"
+echo "Donations are optional http://forum.xda-developers.com/donatetome.php?u=2376614"
+echo "-"
+echo "Recovery taken from http://forum.xda-developers.com/showthread.php?t=1197991"
+echo "-"
+echo "Pushing recovery to /data/local/"
+adb push VM670NH_recovery.img /data/local/recovery.img
+echo "Removing old sensor config, setting up symlink attack"
+adb shell "rm /data/amit/AMI304_Config.ini"
+adb shell "ln -s /dev/block/mtd2 /data/amit/AMI304_Config.ini"
+echo "Rebooting to execute symlink attack, to chmod 666 the recovery block device"
+adb reboot
+adb wait-for-device
+sleep 10
+adb shell "rm /data/amit/AMI304_Config.ini"
+adb shell "dd if=/data/local/recovery.img of=/dev/block/mtd2"
+echo "Recovery flashed! Enjoy"
