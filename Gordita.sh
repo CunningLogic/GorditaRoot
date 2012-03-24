933df076fc5547b2942e05eb2417e5b1ae5ee3ba
Justin Case <jcase@cunninglogic.com>
2012-03-24 00:12:07 -0700

    update

--- Gordita.sh
+++ Gordita.sh
@@ -9,13 +9,16 @@ echo "Recovery taken from http://forum.xda-developers.com/showthread.php?t=11979
 echo "-"
 echo "Pushing recovery to /data/local/"
 adb push VM670NH_recovery.img /data/local/recovery.img
+adb push flash_image /data/local/
+adb shell "chmod 755 /data/local/flash_image"
 echo "Removing old sensor config, setting up symlink attack"
-adb shell "rm /data/amit/AMI304_Config.ini"
-adb shell "ln -s /dev/block/mtdblock2 /data/amit/AMI304_Config.ini"
+adb shell "rm /data/amit/*"
+adb shell "ln -s /dev/mtd/mtd2 /data/amit/AMI304_Config.ini"
+adb shell "ln -s /dev/mtd/mtd2 /data/amit/AMI304_Config2.ini"
 echo "Rebooting to execute symlink attack, to chmod 666 the recovery block device"
 adb reboot
 adb wait-for-device
 sleep 10
-adb shell "rm /data/amit/AMI304_Config.ini"
-adb shell "dd if=/data/local/recovery.img of=/dev/block/mtdblock2"
+adb shell "rm /data/amit/*"
+adb shell "/data/local/flash_image recovery /data/local/recovery.img"
 echo "Recovery flashed! Enjoy"
